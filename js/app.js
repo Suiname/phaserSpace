3668128dd54363a046a3311cd65745c699f5f6b1
Jason Tham <jtham@sbcglobal.net>
2015-11-14 23:11:58 -0600

    added floor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var ninja;
+var platforms;
 function preload() {
 
     game.load.image('brick', 'assets/brick.png');
@@ -15,6 +16,17 @@ for (var i = 0; i < 13; i++) {
     game.add.sprite(0 + i*64, 0+ j*64, 'brick');
   }
 }
+
+platforms = game.add.group();
+platforms.enableBody = true;
+
+var floor;
+for (var i = 0; i < 26; i++) {
+  floor = platforms.create(0 + i*32, game.world.height - 64, 'floor');
+}
+floor.body.immovable = true;
+
+
 ninja = game.add.sprite(50, 50, 'ninja');
 ninja.animations.add('run', [16, 17, 18, 19, 20, 21, 22, 23], 30, true);
 
